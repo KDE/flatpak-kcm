e269bf6797e86ae74fdb278c10491dacfd03ec9c
ivan tkachenko <me@ratijas.tk>
2023-03-21 00:59:34 +0600

    FlatpakPermissionModel: Fix D-Bus "none" policy's i18n, capitalization and loading/saving
    It is spelled lowercase and obviously shall not be translated in config.
    
    Apart from that, "none" is a valid policy mode and a legit effective value.
    
    (cherry picked from commit 2c909f499b13986793236e6ef8d89948bd27d56a)

--- autotests/flatpakpermissiontest.cpp
+++ autotests/flatpakpermissiontest.cpp
@@ -5,6 +5,8 @@
 #include <QMetaEnum>
 #include <QTest>
 
+#include <KConfig>
+#include <KConfigGroup>
 #include <KLocalizedString>
 
 #include "flatpakcommon.h"
@@ -99,6 +101,82 @@ private Q_SLOTS:
         const auto indexOfCustom = filesystems.indexOf(custom);
         QVERIFY(indexOfCustom != -1);
         QVERIFY(indexOfHostEtc < indexOfCustom);
+    }
+
+    void testDBusNonePolicy()
+    {
+        // This test verifies that "none" policy for D-Bus services works just
+        // like other regular policies. It can be loaded, changed to, changed
+        // from, and saved.
+        FlatpakReferencesModel referencesModel;
+        QFile metadataFile(QFINDTESTDATA("fixtures/metadata/org.gnome.dfeet"));
+        QVERIFY(metadataFile.open(QFile::ReadOnly));
+        FlatpakReference reference(&referencesModel,
+                                   "org.gnome.dfeet",
+                                   "x86_64",
+                                   "stable",
+                                   "0.3.16",
+                                   "D-Feet",
+                                   QFINDTESTDATA("fixtures/overrides/"),
+                                   QUrl(),
+                                   metadataFile.readAll());
+        FlatpakPermissionModel model;
+        model.setReference(&reference);
+        model.load();
+        model.setShowAdvanced(true);
+
+        // This service is set to "none" by default (in metadata).
+        const auto service0 = QLatin1String("com.example.service0");
+        int indexOfService0 = -1;
+        // This service is set to "none" in override file.
+        const auto service1 = QLatin1String("com.example.service1");
+        int indexOfService1 = -1;
+        // This service is set to "see" in override, but will be changed to "none".
+        const auto service2 = QLatin1String("com.example.service2");
+        int indexOfService2 = -1;
+
+        for (auto i = 0; i <= model.rowCount(); ++i) {
+            const QString name = model.data(model.index(i, 0), FlatpakPermissionModel::Name).toString();
+            const QString categoryi18n = model.data(model.index(i, 0), FlatpakPermissionModel::Category).toString();
+            const QString category = FlatpakPermission::categoryHeadingToRawCategory(categoryi18n);
+            if (category == QLatin1String(FLATPAK_METADATA_GROUP_SESSION_BUS_POLICY)) {
+                if (name == service0) {
+                    indexOfService0 = i;
+                } else if (name == service1) {
+                    indexOfService1 = i;
+                } else if (name == service2) {
+                    indexOfService2 = i;
+                }
+            }
+        }
+        QVERIFY(indexOfService0 != -1);
+        QVERIFY(indexOfService1 != -1);
+        QVERIFY(indexOfService2 != -1);
+        const auto name0 = model.data(model.index(indexOfService0, 0), FlatpakPermissionModel::EffectiveValue).toString();
+        const auto name1 = model.data(model.index(indexOfService1, 0), FlatpakPermissionModel::EffectiveValue).toString();
+        const auto name2 = model.data(model.index(indexOfService2, 0), FlatpakPermissionModel::EffectiveValue).toString();
+        QCOMPARE(name0, i18n("None"));
+        QCOMPARE(name1, i18n("None"));
+        QCOMPARE(name2, i18n("see"));
+
+        const auto setAndCheckBus = [&](const QString &value, const QString &i18nValue) {
+            model.editPerm(indexOfService2, i18nValue);
+            const auto name2i18nValue = model.data(model.index(indexOfService2, 0), FlatpakPermissionModel::EffectiveValue).toString();
+            QCOMPARE(name2i18nValue, i18nValue);
+
+            QVERIFY(model.isSaveNeeded());
+            model.save();
+
+            const KConfig expectedDesktopFile(QFINDTESTDATA("fixtures/overrides/org.gnome.dfeet"));
+            const auto group = expectedDesktopFile.group(QLatin1String(FLATPAK_METADATA_GROUP_SESSION_BUS_POLICY));
+            const auto name2value = group.readEntry(service2);
+            QCOMPARE(name2value, value);
+        };
+
+        setAndCheckBus(QLatin1String("none"), i18n("None"));
+        setAndCheckBus(QLatin1String("see"), i18n("see"));
+        setAndCheckBus(QLatin1String("talk"), i18n("talk"));
+        setAndCheckBus(QLatin1String("own"), i18n("own"));
     }
 
     void testMutable()
