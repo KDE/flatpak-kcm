aef712f5877d2b3f18d2245e59df99da6eaa61c7
David Redondo <kde@david-redondo.de>
2025-09-17 11:24:05 +0200

    Use kcm_app-permissions translation domain
    The qml part will magically use this with no way to change that

--- Messages.sh
+++ Messages.sh
@@ -2,4 +2,4 @@
 # SPDX-FileCopyrightText: 2022 Suhaas Joshi <joshiesuhaas0@gmail.com>
 # SPDX-License-Identifier: BSD-3-Clause
 
-$XGETTEXT `find . -name "*.cpp" -o -name "*.qml"` -o $podir/kcm_flatpak.pot
+$XGETTEXT `find . -name "*.cpp" -o -name "*.qml"` -o $podir/kcm_app-permissions.pot
